9c18ea48f19ff4a81d83f044d928329d62064721
HermSidhu <h-sidhu@hotmail.com>
2019-10-16 21:58:06 +0000

    positions of letters

---
+++ letterPositions.js
@@ -0,0 +1,43 @@
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== ' ') {
+      if (results[sentence[i]]) {
+        results[sentence[i]].push(i)
+      } else {
+        results[sentence[i]] = [i];
+      }
+    }
+  }
+  return results;
+};
+console.log(letterPositions('Lighthouse Labs'))
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i] && (!Array.isArray(array1[i]) || !Array.isArray(array2[i]))) {
+      return false;
+    }
+    if (Array.isArray(array1[i]) && Array.isArray(array2[i])) {
+      if (!eqArrays(array1[i], array2[i])) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  const result = eqArrays(array1,array2);
+  if (result) {
+    console.log(`✅ ✅ ✅  Assertion passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`❌ ❌ ❌  Assertion failed: ${array1} !== ${array2}`);
+  }
+};
+
+assertArraysEqual(letterPositions('hello').e, [1])
